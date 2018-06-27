05af116eaa9e7eceb6f632fe2ee18e7b7dd6f13f
Jeshua Ensong <jeshuae@codev.com>
2018-06-27 09:12:01 +0800

    Commit changes

--- www/assessment/items/itemsapi_assess.php
+++ www/assessment/items/itemsapi_assess.php
@@ -12,15 +12,17 @@ $security = array(
 );
 
 $request = [
-    'activity_id' => '1c658d55-68cf-488e-9f4d-3096fffcb7a4',
+    // 'activity_id' => '1c658d55-68cf-488e-9f4d-3096fffcb7a4',
+    'activity_id' => 'c58a0553-0eef-42c2-9d32-fcb1e3af767b',
     'name' => 'Items API demo - assess activity',
     'rendering_type' => 'assess',
     'type' => 'submit_practice',
-    'session_id' => Uuid::generate(),
+    'session_id' => 'C501741A-CA4A-4DC8-A4F2-FF0B1C6FA817',
+    // 'session_id' => Uuid::generate(),
     // 'session_id' => '9ac6c233-8786-474e-b180-94dcf726a9f4',
     'user_id' => $studentid,
     'items' => [
-        'Demo3',
+        // 'Demo3',
         // 'Demo4',
         // 'accessibility_demo_6',
         // 'Demo6',
@@ -29,22 +31,23 @@ $request = [
         // 'Demo9',
         // 'Demo10',
         // 'audioplayer-demo-1'
-        '06b16d71-9971-4254-9f8f-44cc825799ac',
-        'b3206d89-1fca-4485-ba9d-a338f73abdb6',
-        'ee213ba1-a821-48b8-8cb1-9600eb84eff0',
-        '0fa2de9a-1ac6-449c-9f11-aee116929210',
-        '2c6ed8e5-e6d2-479d-9c9b-b0d41b66391d',
-        '248df79d-ef24-4b26-b20f-7abce464f2b8',
-        '5d5a7917-12c3-446e-9c1f-15d932ce287e',
-        'c42dd5d1-8578-461a-acfa-0c03556bc0b0',
-        'a568513a-1e74-4247-9fe7-e0819eccbdf0',
-        '7d6d2056-28b4-4c0f-ad41-250adb65928b',
-        'bbca3e1e-1736-4d85-bc13-d5f91aa1adec',
-        '4e7d3d3e-2a3b-40fc-94fb-99d349044e9b',
-        '8c43e434-9c43-4ea8-8bce-4b0cd648e8c5',
-        '18516fef-d3b7-4a0e-ba54-34e59d068b30',
-        '32fc7b0a-4228-49f7-bde6-ee1846314627',
-        '686bbf58-68eb-4959-804c-1fe4f17c9ba5'
+        // '06b16d71-9971-4254-9f8f-44cc825799ac',
+        // 'b3206d89-1fca-4485-ba9d-a338f73abdb6',
+        // 'ee213ba1-a821-48b8-8cb1-9600eb84eff0',
+        // '0fa2de9a-1ac6-449c-9f11-aee116929210',
+        // '2c6ed8e5-e6d2-479d-9c9b-b0d41b66391d',
+        // '248df79d-ef24-4b26-b20f-7abce464f2b8',
+        // '5d5a7917-12c3-446e-9c1f-15d932ce287e',
+        // 'c42dd5d1-8578-461a-acfa-0c03556bc0b0',
+        // 'a568513a-1e74-4247-9fe7-e0819eccbdf0',
+        // '7d6d2056-28b4-4c0f-ad41-250adb65928b',
+        // 'bbca3e1e-1736-4d85-bc13-d5f91aa1adec',
+        // '4e7d3d3e-2a3b-40fc-94fb-99d349044e9b',
+        // '8c43e434-9c43-4ea8-8bce-4b0cd648e8c5',
+        // '18516fef-d3b7-4a0e-ba54-34e59d068b30',
+        // '32fc7b0a-4228-49f7-bde6-ee1846314627',
+        // '686bbf58-68eb-4959-804c-1fe4f17c9ba5'
+        "5d5a7917-12c3-446e-9c1f-15d932ce287e","bbca3e1e-1736-4d85-bc13-d5f91aa1adec","ee213ba1-a821-48b8-8cb1-9600eb84eff0","7d6d2056-28b4-4c0f-ad41-250adb65928b","0fa2de9a-1ac6-449c-9f11-aee116929210","248df79d-ef24-4b26-b20f-7abce464f2b8","b3206d89-1fca-4485-ba9d-a338f73abdb6","2c6ed8e5-e6d2-479d-9c9b-b0d41b66391d","8c43e434-9c43-4ea8-8bce-4b0cd648e8c5","a568513a-1e74-4247-9fe7-e0819eccbdf0","06b16d71-9971-4254-9f8f-44cc825799ac","c42dd5d1-8578-461a-acfa-0c03556bc0b0","4e7d3d3e-2a3b-40fc-94fb-99d349044e9b","B2F7888E-FD60-4991-B839-761ADF58B6BE","D055E493-EE52-42BA-AAFE-BDB5E05F5E8A","DF314130-2C80-41DF-BC34-9FA0326BAAD8"
     ],
 ];
 
